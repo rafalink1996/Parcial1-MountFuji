2beefc494f520e7da260c2612115800f37a4f728
Rafael Ospina <rafaelospiname@gmail.com>
2018-02-28 15:16:30 -0500

    Terminado. Con nubes

---
+++ RafaelOspina-MountFuji/Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float speedX;
+	public float speedY;
+	public float speedZ;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate (speedX, speedY, speedZ);
+		
+	}
+}
